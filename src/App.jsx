69f9ab8a9901a44c0cf2d573f4e9c7fdd228b78a
Oleksandr <al.tepl@icloud.com>
2025-11-29 19:18:46 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
-import BlogCard from "./components/BlogCard";   // імпорт компонента
-import "./index.css";                           // глобальні стилі
+import BlogCard from "./components/BlogCard";
+import "./index.css";
+
+// Імпорт файлів (правильний спосіб!)
+import blogImage from "./assets/images/BlogImage.png";
+import avatarMale from "./assets/images/avatar_male.png";
 
 export default function App() {
   return (
     <main>
       <BlogCard
-        image="/assets/images/BlogImage.png"
+        image={blogImage}
         category="HTML / CSS"
         title="Практична №3"
         date="Публікація 21 вересня 2025"
         description="Мета практичної роботи полягає в тому, щоб налаштувати середовище розробки VS Code, а також реалізувати картку блогу для вдосконалення фундаментальних навичок з HTML та CSS."
         author="Тепляков Олександр Дмитрович"
-        avatar="/assets/images/avatar_male.png"
+        avatar={avatarMale}
       />
     </main>
   );
